0353d17e5b8bd7915c642b4ad056a42791c5adc7
Satyendra singh <57335118+satyendrasingh8@users.noreply.github.com>
2020-05-19 21:18:51 +0530

    set up dependencies and modify dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ return {
 
     entry:'./src/app.js',
     output: {
-     path:path.join(__dirname,'public'),
+     path:path.join(__dirname,'public','dist'),
      filename:'bundle.js'
    
     },
@@ -49,7 +49,8 @@ return {
     devtool:isProduction ? 'source-map' : 'inline-source-map',
     devServer:{
     contentBase:path.join(__dirname,'public'),
-    historyApiFallback:true
+    historyApiFallback:true,
+    publicPath:'/dist/'
     }
    
    
