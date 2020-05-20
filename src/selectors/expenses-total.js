8ed9b5f7ab480caaf87984fed40f55c76773c9e1
Satyendra singh <57335118+satyendrasingh8@users.noreply.github.com>
2020-05-20 13:31:48 +0530

    viewing total amount and expenses

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,13 +1,9 @@
-import expenses from '../tests/fixtures/expenses';
- const selectExpensesTotal = (expenses) => {
-if (expenses.length===0) {
-return 0;
-}
-else {
-return expenses.map((expense) => expense.amount).reduce( (sum,value)=> {
-return sum+value;
-},0 );
-}
+//import expenses from '../tests/fixtures/expenses';
+
+ export default (expenses) => {
+return expenses
+.map((expense) => expense.amount)
+.reduce( (sum,value)=>sum+value, 0 );
+
 };
 
-export default  selectExpensesTotal;
